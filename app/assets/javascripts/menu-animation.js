4adb1e604e77a80461e299364a037437dd59bb01
omar-sr88 <omar.sr88@gmail.com>
2016-07-27 17:28:58 -0300

    added menu toggle, getting butt kicked by css

--- app/assets/javascripts/menu-animation.js
+++ app/assets/javascripts/menu-animation.js
@@ -1,28 +1,7 @@
-$(document).ready(function () {
-  var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
-
-    trigger.click(function () {
-      hamburger_cross();      
-    });
-
-    function hamburger_cross() {
-
-      if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
-  }
-  
-  $('[data-toggle="offcanvas"]').click(function () {
+var ready = function() {
+  $("#menu-toggle").click(function () {
         $('#wrapper').toggleClass('toggled');
   });  
-});
+};
+
+$(document).on('turbolinks:load', ready);
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'home', to: 'static_pages#home' , as: 'home'
 
-  get 'static_pages/work'
+  get '/work' , to: 'static_pages#work', as: 'work'
 
-  get 'static_pages/education'
+  get '/education' , to: 'static_pages#education' , as: 'education'
 
-  get 'static_pages/interests'
+  get '/interests' , to: 'static_pages#interests' , as: 'interests'
 
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
