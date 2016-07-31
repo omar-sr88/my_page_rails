f7962341078ca4977a6e9c50d151e1cecbabb3f3
omar-sr88 <omar.sr88@gmail.com>
2016-07-31 12:58:15 -0300

    Work, interests, tab division

--- app/assets/javascripts/menu-animation.js
+++ app/assets/javascripts/menu-animation.js
@@ -2,6 +2,20 @@ var ready = function() {
   $("#menu-toggle").click(function () {
         $('#wrapper').toggleClass('toggled');
   });  
+
+  $('ul.tabs li:first').addClass('active');
+  $('article').hide();
+  $('article:first').show();
+
+  $('ul.tabs li').on('click',function(){
+    $('ul.tabs li').removeClass('active');
+    $(this).addClass('active')
+    $('article').hide();
+    var activeTab = $(this).find('a').attr('href');
+    $(activeTab).show();
+    return false;
+  });
+
 };
 
 $(document).on('turbolinks:load', ready);
